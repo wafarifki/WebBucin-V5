b65d5339cafe0adaa53e280bc0257f43ccccaf09
Wafa Rifqi Anafin <71179459+wafarifki@users.noreply.github.com>
2022-10-25 16:42:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Sore Sayangku", "Aku Sayang Banget Sama Kamu", "I Love You Annisa Aulia"],
+        typeSpeed: 70
+    });
+}
